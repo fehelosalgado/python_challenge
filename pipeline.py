9d27cf272ef8ddc07568066ee6a279e5a9088cd6
“Felipe <fehelosalgado@gmail.com~>
2026-02-12 17:29:39 -0300

    remoção do trecho de código [if __name__ == "__main__":] antes de executar a chamada da função que baixa o arquivo json do blob no pipeline principal

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,4 @@
 from src.bronze.ingest_bronze import download_jira_data
 
-if __name__ == "__main__":
-    download_jira_data()
+# baixa o arquivo json do blob para a camada bronze (traduzir para o ingles)
+download_jira_data()
